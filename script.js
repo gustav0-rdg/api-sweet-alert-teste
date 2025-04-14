a57b0fad3b3a811eec56b142e3854ccb13b2516d
gustavo rodrigues <gustavolourenco821@gmail.com>
2025-04-14 09:45:45 -0300

    funcao de alerta sucesso

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('alertSucesso').addEventListener('click', () =>{
+    Swal.fire({
+        title: 'Sucesso!', 
+        text: 'A operação foi concluída com sucesso', 
+        icon: 'success'
+    })
+});
